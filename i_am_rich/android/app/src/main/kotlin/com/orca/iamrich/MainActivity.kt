e0aab39f2d408287851db3f44b33bb2728812a72
Orca <generaljemmy.mg@gmail.com>
2020-06-04 15:35:42 +0200

    i_am_rich_app

---
+++ i_am_rich/android/app/src/main/kotlin/com/orca/iamrich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.orca.iamrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.amber,
+        body: Center(
+          child: Image(
+//              image: NetworkImage(
+//                  'https://cdn.eso.org/images/thumb300y/eso1907a.jpg'),
+//            ),
+            image: AssetImage('images/icon.png'),
+          ),
+        ),
+      ),
+    ));
